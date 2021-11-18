d00cc52850c836c52bc6b3324ef35b5423af630c
TedYen <da9944208810722@gmail.com>
2021-11-18 17:50:58 +0800

    增加Ticket相關路由

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,5 +21,6 @@ Route::get('/', function () {
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
-
+Route::apiResource('ticket',TicketController::class);
 require __DIR__.'/auth.php';
+

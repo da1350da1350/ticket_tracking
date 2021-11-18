3aeb9b18ab3a4bb42eb143785fab0f497933af4a
TedYen <da9944208810722@gmail.com>
2021-11-18 17:53:49 +0800

    增加Ticket 相關Request(初版)

---
+++ app/Http/Requests/UpdateTicketRequest copy.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTicketRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return User::hasRole('QA');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}

9ab043b32756dfe368bb0cf0f9759cafac64848a
TedYen <da9944208810722@gmail.com>
2021-11-19 13:44:05 +0800

    調整路由 增加resolve路由

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TicketController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,4 +18,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::patch('/ticket/resolve/{id}',"TicketController@resolve")->name("ticket.resolve");
+Route::patch('/ticket/resolve/{id}', [TicketController::class,'resolve'])->name("ticket.resolve");
